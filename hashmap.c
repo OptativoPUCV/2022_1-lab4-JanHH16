3133c3d1557c194851a1d804c6ca72f9285dbce0
JanHH16 <>
2022-04-08 15:15:35 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -53,7 +53,7 @@ void enlarge(HashMap * map) {
 
 HashMap * createMap(long capacity)
 {
-    HashMap *map = (HashMap *) malloc(sizeof(capacity));
+    HashMap *map = (HashMap *) malloc(sizeof(HashMap));
     map[capacity] = NULL;
   
   
