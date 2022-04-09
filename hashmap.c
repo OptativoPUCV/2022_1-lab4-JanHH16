815fcbccb720d5b51735b90164f824fbeeb4496e
Your Name <you@example.com>
2022-04-09 17:15:48 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -93,7 +93,7 @@ Pair * searchMap(HashMap * map,  char * key)
         {
             map -> current = hashPosicion;
             bloque = map -> buckets[hashPosicion];
-            return bloque;
+            return key;
         }
         else
         {
